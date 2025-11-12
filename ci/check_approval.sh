3c99369935b6f4cde62541d95eebce9097697d6d
gushiwei <gushiwei@baidu.com>
2025-11-12 03:09:30 +0000

    add approval

--- ci/check_approval.sh
+++ ci/check_approval.sh
@@ -34,7 +34,7 @@ echo_list=()
 
 function check_approval(){
     person_num=`echo $@|awk '{for (i=2;i<=NF;i++)print $i}'`
-    APPROVALS=`echo ${approval_line}|python ${PADDLE_ROOT}/tools/check_pr_approval.py $1 $person_num`
+    APPROVALS=`echo ${approval_line}|python ${PADDLE_ROOT}/ci/check_pr_approval.py $1 $person_num`
     if [[ "${APPROVALS}" == "FALSE" && "${echo_line}" != "" ]]; then
         add_failed "${failed_num}. ${echo_line}"
     fi
