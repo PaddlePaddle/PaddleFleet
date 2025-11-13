39470732d098409638c6efff4598d3393c32cae8
gushiwei <gushiwei@baidu.com>
2025-11-13 04:13:18 +0000

    add disable test

---
+++ ci/single_card_test.sh
@@ -0,0 +1,28 @@
+# Copyright (c) 2025 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+disable_file="$work_dir/tests/multi_card_tests/disable_single_card_uts.txt"
+
+disabled=()
+if [ -f "$disable_file" ]; then
+    while IFS= read -r line; do
+        [[ -z "$line" || "$line" =~ ^# ]] && continue
+        filename=$(basename "$line")
+        disabled+=("$filename")
+    done < "$disable_file"
+fi
+
+echo "Disabled tests: ${disabled[@]}"
+
+uv run pytest tests/single_card_tests $(sed 's/^/--ignore=/' tests/single_card_tests/disable_single_card_uts.txt)
