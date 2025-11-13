311cc740adfa37ddfe266cb857971bca39e26b40
blacksheep-Aristotle <zhangweilong01@baidu.com>
2025-11-13 14:18:05 +0800

    add mlp layer

--- fleet/core/jit.py
+++ fleet/core/jit.py
@@ -11,6 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+# Copyright (c) 2024, NVIDIA CORPORATION. All rights reserved.
+
 import paddle
 
 jit_fuser = paddle.jit.to_static(backend="CINN")
