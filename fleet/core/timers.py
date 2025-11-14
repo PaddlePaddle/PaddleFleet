d636a62963c410f1e99f93159c3dca2a515f3c61
huangjiyi <947613776@qq.com>
2025-11-14 03:14:05 +0000

    update

--- fleet/core/timers.py
+++ fleet/core/timers.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Refer to NVIDIA Megatron-LM https://github.com/NVIDIA/Megatron-LM.git
-# Copyright (c) 2023, NVIDIA CORPORATION. All rights reserved.
 from __future__ import annotations
 
 import time
