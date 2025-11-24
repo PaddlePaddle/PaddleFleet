8ecb124308c61d57156f15f8181623fe5abb9269
gushiwei <gushiwei@baidu.com>
2025-11-24 14:25:00 +0000

    add integration test glm

---
+++ ci/integration_test/glm45.sh
@@ -0,0 +1,63 @@
+# Copyright (c) 2025 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+root_dir=$(pwd)
+
+cd PaddleFleet
+uv sync -v
+source .venv/bin/activate
+
+cd ../PaddleFormers
+pip install -e .
+cd examples/experiments/paddlefleet
+
+wget -q --tries=5 --no-proxy https://xly-devops.cdn.bcebos.com/PaddleFleet/glm45_dataset/data.tar --no-check-certificate
+tar -xf data.tar
+
+apt-get update
+apt-get install jq
+
+jq '.expert_parallel_degree = 8' glm45.json > glm45_single_node.json
+sed -i 's/from paddlefleet\.transformer import LayerSpec/from paddlefleet import LayerSpec/' glm45_provider.py
+sed -i 's/from paddlefleet\.transformer import LayerSpec/from paddlefleet import LayerSpec/' gpt_provider.py
+sed -i '/if not int(os.getenv("test_ci_no_save_model", 0)):/s/^/# /' run_pretrain.py
+sed -i '/trainer.save_model()/s/^/# /' run_pretrain.py
+sed -i 's/num_layers: int = 10/num_layers: int = 5/' glm45_provider.py
+
+python3 -c "
+infile = '$root_dir/PaddleFormers/paddleformers/trainer/training_args.py'
+outfile = infile + '.new'
+with open(infile) as fin, open(outfile, 'w') as fout:
+    for line in fin:
+        fout.write(line)
+        if line.strip() == '# initialize_fleet(strategy)':
+            pad = line[:len(line) - len(line.lstrip())]
+            fout.write(pad + 'import paddlefleet\n')
+            fout.write(pad + 'paddlefleet.training.initialize.initialize_fleet(strategy)\n')
+"
+mv $root_dir/PaddleFormers/paddleformers/trainer/training_args.py.new $root_dir/PaddleFormers/paddleformers/trainer/training_args.py
+
+rm -rf checkpoint/
+rm -rf outputs/
+master=$(hostname -i)
+port=36677
+
+python -m paddle.distributed.launch \
+   --log_dir ./log \
+   --master $master:$port \
+   --nnodes 1 \
+   --rank 0 \
+   --run_mode=collective \
+   run_pretrain.py glm45_single_node.json \
+   --output_dir ./checkpoint
