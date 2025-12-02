8b3b30853203e077ad561feaf6bde3656385346d
xiaoguoguo626807 <wangruting@baidu.com>
2025-12-02 19:57:10 +0800

    router to gate

--- src/paddlefleet/transformer/moe/moe_layer.py
+++ src/paddlefleet/transformer/moe/moe_layer.py
@@ -116,7 +116,7 @@ class MoELayer(nn.Layer):
         self.expert_dropout = config.get("expert_dropout", 0.0)
 
         self._init_expert_parallel()
-        self.router = StandardMoERouter(
+        self.gate = StandardMoERouter(
             config=config, pg_collection=pg_collection
         )
 
@@ -292,7 +292,7 @@ class MoELayer(nn.Layer):
             priorities,
             aux_loss,
             z_loss,
-        ) = self.router(hidden_states)
+        ) = self.gate(hidden_states)
         # topk_weights, topk_indices will be used in AllToAllMoECommunication
         # gates_masked, mask will be used in DeepEPMoECommunication
         # capacity, priorities are not used currently
