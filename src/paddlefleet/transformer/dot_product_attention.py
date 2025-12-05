819a6af9e6e8ff60c2d5ad57e2a90e92b96ba444
xiaoguoguo626807 <wangruting@baidu.com>
2025-12-05 14:52:05 +0800

    fix eagerparam has no main_grad

--- src/paddlefleet/transformer/dot_product_attention.py
+++ src/paddlefleet/transformer/dot_product_attention.py
@@ -194,7 +194,7 @@ class DotProductAttention(FleetLayer):
                 query,
                 key,
                 value,
-                None,  # attention_mask
+                attention_mask,
                 self.config.attention_dropout,
                 is_causal=True,
                 training=True,
