62696de7ab019a30b8db0403da5b6232841e1a12
Wennie396 <wn295448@buaa.edu.cn>
2025-12-10 18:58:51 +0800

    fix

--- tests/multi_card_tests/test_gpt_model_dense_cp.py
+++ tests/multi_card_tests/test_gpt_model_dense_cp.py
@@ -152,8 +152,8 @@ def run_cp(seed, batch_size, seq_len, vocab_size, config):
     # )
     # loss = outputs[0]
     # loss.backward()
-    print(f"############## {loss.item()}")
-    loss_baseline = 7.186676
+    # print(f"############## {loss.item()}")
+    loss_baseline = 7.186675548553467
     np.testing.assert_allclose(
         np.array(loss), np.array(loss_baseline), rtol=1e-6, atol=1e-8
     )
