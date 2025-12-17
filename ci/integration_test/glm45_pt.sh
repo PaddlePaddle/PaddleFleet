2ebbf252be6dba2cb0d78e81220b21c3c5217cef
gushiwei <gushiwei@baidu.com>
2025-12-17 13:41:52 +0000

    add sft+lora

--- ci/integration_test/glm45_pt.sh
+++ ci/integration_test/glm45_pt.sh
@@ -17,48 +17,39 @@ export root_dir=$(pwd)
 
 wget https://github.com/mikefarah/yq/releases/download/v4.44.1/yq_linux_amd64 -O /usr/local/bin/yq
 chmod +x /usr/local/bin/yq
-apt-get update
-apt-get install jq -y
 
 source PaddleFleet/.venv/bin/activate
 
-wget -q --tries=5 --no-proxy https://xly-devops.cdn.bcebos.com/PaddleFleet/glm45/glm45_fleet_pt.1214.tar --no-check-certificate
-tar -xf glm45_fleet_pt.1214.tar # glm45_fleet_pt
-cd $root_dir/glm45_fleet_pt
+wget -q --tries=5 --no-proxy https://xly-devops.cdn.bcebos.com/PaddleFleet/glm45/glm45_fleet.1217.tar --no-check-certificate
+tar -xf glm45_fleet.1217.tar # glm45_fleet
+cd $root_dir/glm45_fleet
 export cur_dir=$(pwd)
 
-config_yaml=$cur_dir/glm45.yaml
-config_json=${cur_dir}/GLM-4.5-Air/config.json
+config_yaml=$cur_dir/glm45_pt.yaml
 
-yq eval '.expert_model_parallel_size = 1
+yq eval '.expert_model_parallel_size = 8
     | .gradient_accumulation_steps = 1
-    | .per_device_train_batch_size = 1
-    | .use_expert_parallel = false
     | .train_dataset_path = strenv(cur_dir) + "/data/pre-training/train.jsonl"
     | .eval_dataset_path = strenv(cur_dir) + "/data/pre-training/eval.jsonl"
     | .model_name_or_path = strenv(cur_dir) + "/GLM-4.5-Air"
     | .logging_dir = strenv(cur_dir) + "/vdl_log"
     | .output_dir = strenv(cur_dir) + "/checkpoints"' \
-  $config_yaml > ${config_yaml}.tmp
+   $config_yaml > ${config_yaml}.tmp
 mv ${config_yaml}.tmp $config_yaml
 
-# jq --arg cur_dir "$cur_dir" \
-#     '.first_k_dense_replace = 0' \
-#     $config_json > ${config_json}.tmp
-# mv ${config_json}.tmp $config_json
+sed -i 's/config.num_hidden_layers = 10/config.num_hidden_layers = 3/g' $root_dir/PaddleFormers/paddleformers/transformers/glm4_moe/modeling.py
+sed -i 's/\[0\] \* 1 + \[1\] \* 9/\[0\] \* 1 + \[1\] \* 2/g' $root_dir/PaddleFormers/paddleformers/transformers/glm4_moe/modeling.py
 
-sed -i 's/config.num_hidden_layers = 10/config.num_hidden_layers = 2/g' /workspace/PaddleFormers/paddleformers/transformers/glm4_moe/modeling.py
-sed -i 's/\[0\] \* 1 + \[1\] \* 9/\[0\] \* 1 + \[1\] \* 1/g' /workspace/PaddleFormers/paddleformers/transformers/glm4_moe/modeling.py
-
-rm -rf checkpoints/
-rm -rf vdl_log/
+rm -rf ./outputs
+rm -rf paddleformers_dist_log
 master=$(hostname -i)
 port=36677
 
-export FLAGS_embedding_deterministic=1
-export FLAGS_cudnn_deterministic=1
+export FLAGS_use_stride_compute_kernel=False
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 
 unset http_proxy https_proxy
+
 # coverage run -m paddle.distributed.launch \
 #    --log_dir ./log \
 #    --master $master:$port \
@@ -66,6 +57,15 @@ unset http_proxy https_proxy
 #    --rank 0 \
 #    --run_mode=collective \
 #    run_pretrain.py $config_json \
-#    --output_dir ./checkpoint | tee ./glm45_a100.log
+#    --output_dir ./checkpoint 2>&1 | tee ./glm45.log
 
-FLAGS_use_stride_compute_kernel=False NNODES=1 MASTER_ADDR=$master MASTER_PORT=$port CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 coverage run $(which paddleformers-cli) train $cur_dir/glm45.yaml 2>&1 | tee ./glm45_a100.log
+NNODES=1 MASTER_ADDR=$master MASTER_PORT=$port coverage run $(which paddleformers-cli) train $config_yaml 2>&1 | tee ./glm45_pt.log
+
+echo "
+10 11.70267296
+" > ./glm45_pt_multi_card_gt_loss.txt
+
+python $root_dir/PaddleFleet/ci/integration_test/check_loss.py \
+   --compare_step 10 \
+   --log_file ./glm45_pt.log \
+   --gt_file ./glm45_pt_multi_card_gt_loss.txt
---
+++ ci/integration_test/glm45_sft.sh
@@ -0,0 +1,57 @@
+# Copyright (c) 2025 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -exo pipefail
+export root_dir=$(pwd)
+
+cd $root_dir/PaddleFormers
+git reset --hard HEAD
+cd -
+
+apt-get update
+apt-get install jq -y
+
+cd $root_dir/glm45_fleet
+export cur_dir=$(pwd)
+
+config_sft_yaml=$cur_dir/tp4_sp_ep4_pp2_sft.yaml
+config_lora_yaml=$cur_dir/tp4_sp_ep4_pp2_lora.yaml
+
+config_json=$CACHE_DIR/glm45/GLM-4.5-Air/config.json
+
+yq eval '.train_dataset_path = strenv(cur_dir) + "/data/sft/train_gsm8k.jsonl"
+    | .eval_dataset_path = strenv(cur_dir) + "/data/sft/test_gsm8k.jsonl"
+    | .model_name_or_path = strenv(CACHE_DIR) + "/glm45/GLM-4.5-Air"
+    | .logging_dir = strenv(cur_dir) + "/vdl_log"
+    | .output_dir = strenv(cur_dir) + "/checkpoints"' \
+   $config_sft_yaml > ${config_sft_yaml}.tmp
+mv ${config_sft_yaml}.tmp $config_sft_yaml
+
+jq '.num_hidden_layers = 4' \
+    $config_json > ${config_json}.tmp
+mv ${config_json}.tmp $config_json
+
+rm -rf ./outputs
+rm -rf paddleformers_dist_log
+master=$(hostname -i)
+port=36677
+
+export FLAGS_use_stride_compute_kernel=False
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+
+unset http_proxy https_proxy
+
+NNODES=1 MASTER_ADDR=$master MASTER_PORT=$port coverage run $(which paddleformers-cli) train $config_sft_yaml 2>&1 | tee ./glm45_sft.log
+
+NNODES=1 MASTER_ADDR=$master MASTER_PORT=$port coverage run $(which paddleformers-cli) train $config_lora_yaml 2>&1 | tee ./glm45_lora.log
