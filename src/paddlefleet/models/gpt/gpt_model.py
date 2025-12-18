0aaae02745a82d707b154bfbbffdad3f280ec5ae
xiaoguoguo626807 <100397923+xiaoguoguo626807@users.noreply.github.com>
2025-12-18 19:53:43 +0800

    Update src/paddlefleet/models/gpt/gpt_model.py

--- src/paddlefleet/models/gpt/gpt_model.py
+++ src/paddlefleet/models/gpt/gpt_model.py
@@ -152,7 +152,7 @@ class GPTModel(PipelineLayer):
                     spec.lm_head,
                     shared_weight_attr="embedding_weight",
                 ),
-                "shared_head",
+                "model.shared_head",
             )
         else:
             self.add_sequential_layer(
