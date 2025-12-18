00b16d80a4d24778f581e025e67fcb8f3388bfb0
gushiwei <gushiwei@baidu.com>
2025-12-18 05:07:04 +0000

    add sft+lora

--- ci/integration_test/glm45_sft_a100.sh
+++ ci/integration_test/glm45_sft_a100.sh
@@ -33,6 +33,7 @@ config_lora_yaml=$cur_dir/tp4_sp_ep4_pp2_lora.yaml
 config_json=$CACHE_DIR/glm45/GLM-4.5-Air/config.json
 
 yq '.recompute_granularity = ""
+    | .moe_token_dispatcher_type = "alltoall"
     | .gated_linear_unit = true
     | .train_dataset_path = strenv(cur_dir) + "/data/sft/train_gsm8k.jsonl"
     | .eval_dataset_path = strenv(cur_dir) + "/data/sft/test_gsm8k.jsonl"
@@ -43,6 +44,7 @@ yq '.recompute_granularity = ""
 mv ${config_sft_yaml}.tmp $config_sft_yaml
 
 yq '.recompute_granularity = ""
+    | .moe_token_dispatcher_type = "alltoall"
     | .gated_linear_unit = true
     | .train_dataset_path = strenv(cur_dir) + "/data/sft/train_gsm8k.jsonl"
     | .eval_dataset_path = strenv(cur_dir) + "/data/sft/test_gsm8k.jsonl"
