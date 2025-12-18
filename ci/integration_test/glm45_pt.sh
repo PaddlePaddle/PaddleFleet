a6a92c092331a9b6f1c4815c1356a60bfc83b4e6
gushiwei <gushiwei@baidu.com>
2025-12-18 02:41:33 +0000

    add sft+lora

--- ci/integration_test/glm45_pt.sh
+++ ci/integration_test/glm45_pt.sh
@@ -29,6 +29,8 @@ config_yaml=$cur_dir/glm45_pt.yaml
 
 yq eval '.expert_model_parallel_size = 8
     | .gradient_accumulation_steps = 1
+    | .moe_token_dispatcher_type = "deepep"
+    | .gated_linear_unit = true
     | .train_dataset_path = strenv(cur_dir) + "/data/pre-training/train.jsonl"
     | .eval_dataset_path = strenv(cur_dir) + "/data/pre-training/eval.jsonl"
     | .model_name_or_path = strenv(cur_dir) + "/GLM-4.5-Air"
--- ci/integration_test/glm45_sft.sh
+++ ci/integration_test/glm45_sft.sh
@@ -32,13 +32,23 @@ config_lora_yaml=$cur_dir/tp4_sp_ep4_pp2_lora.yaml
 
 config_json=$CACHE_DIR/glm45/GLM-4.5-Air/config.json
 
-yq eval '.train_dataset_path = strenv(cur_dir) + "/data/sft/train_gsm8k.jsonl"
+yq 'del(.recompute_granularity)
+    | .train_dataset_path = strenv(cur_dir) + "/data/sft/train_gsm8k.jsonl"
     | .eval_dataset_path = strenv(cur_dir) + "/data/sft/test_gsm8k.jsonl"
     | .model_name_or_path = strenv(CACHE_DIR) + "/glm45/GLM-4.5-Air"
     | .logging_dir = strenv(cur_dir) + "/vdl_log"
     | .output_dir = strenv(cur_dir) + "/checkpoints"' \
    $config_sft_yaml > ${config_sft_yaml}.tmp
 mv ${config_sft_yaml}.tmp $config_sft_yaml
+
+yq 'del(.recompute_granularity)
+    | .train_dataset_path = strenv(cur_dir) + "/data/sft/train_gsm8k.jsonl"
+    | .eval_dataset_path = strenv(cur_dir) + "/data/sft/test_gsm8k.jsonl"
+    | .model_name_or_path = strenv(CACHE_DIR) + "/glm45/GLM-4.5-Air"
+    | .logging_dir = strenv(cur_dir) + "/vdl_log"
+    | .output_dir = strenv(cur_dir) + "/checkpoints"' \
+   $config_lora_yaml > ${config_lora_yaml}.tmp
+mv ${config_lora_yaml}.tmp $config_lora_yaml
 
 jq '.num_hidden_layers = 4' \
     $config_json > ${config_json}.tmp
