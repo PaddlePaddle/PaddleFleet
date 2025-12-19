8fe3a6c1dc04f01d903cd3d267daeb3a704d3d07
blacksheep-Aristotle <zhangweilong01@baidu.com>
2025-12-19 14:28:54 +0800

    change ci baseline

--- ci/integration_test/glm45.sh
+++ ci/integration_test/glm45.sh
@@ -66,7 +66,7 @@ unset http_proxy https_proxy
 FLAGS_use_stride_compute_kernel=False NNODES=1 MASTER_ADDR=$master MASTER_PORT=$port CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 coverage run $(which paddleformers-cli) train $cur_dir/glm45.yaml 2>&1 | tee ./glm45.log
 
 echo "
-10 11.70267677
+10 11.70315266
 " > ./glm45_multi_card_gt_loss.txt
 
 python $root_dir/PaddleFleet/ci/integration_test/check_loss.py \
