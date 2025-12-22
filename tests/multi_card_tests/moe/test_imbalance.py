5fabdbb4777ee300aece8efe9448f6c24044dbc8
Shenwei Hu <hushenwei@baidu.com>
2025-12-22 16:16:28 +0800

    restore ci loss value

--- tests/multi_card_tests/moe/test_imbalance.py
+++ tests/multi_card_tests/moe/test_imbalance.py
@@ -83,6 +83,7 @@ class TestFusionBF16ExpertParallel(unittest.TestCase):
             hidden_act=F.silu,
             moe_grouped_gemm=True,
             bias_activation_fusion=True,
+            moe_deep_gemm=False,
         )
 
         transformer_layer_spec = get_gpt_layer_local_spec(
