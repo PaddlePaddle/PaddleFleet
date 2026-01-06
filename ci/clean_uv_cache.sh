138d3ea9118d07cf6336c13a501705ccec2e7d7b
gushiwei <gushiwei@baidu.com>
2026-01-06 10:46:21 +0000

    fix

--- ci/clean_uv_cache.sh
+++ ci/clean_uv_cache.sh
@@ -15,6 +15,7 @@
 day=$(date +%d)
 
 if [[ "$day" == "10" || "$day" == "20" || "$day" == "30" ]]; then
+    uv cache clean paddlepaddle-gpu
     uv cache prune
     dir="/root/.cache/uv/builds-v0"
     if [ -d "$dir" ]; then
