e68a7c077d3cf706dc3524590463021f8a0de3e0
Eddie-Wang1120 <wangjinheng1120@163.com>
2026-01-12 15:06:05 +0800

    support fused_transpose_split_quant

--- tests/single_card_tests/custom_ops/test_ops_import.py
+++ tests/single_card_tests/custom_ops/test_ops_import.py
@@ -27,6 +27,7 @@ class TestOpsImport(unittest.TestCase):
         "fused_swiglu_scale",
         "fused_swiglu_scale_bwd",
         "fused_swiglu_bwd",
+        "fused_transpose_split_quant_custom",
     ]
 
     def setUp(self):
