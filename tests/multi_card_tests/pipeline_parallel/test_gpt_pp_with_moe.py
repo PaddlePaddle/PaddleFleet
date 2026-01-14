85c509eb3a2384a62b16b8f4a72aa288729c72a2
YuangLiu <liuyuang@baidu.com>
2026-01-15 06:37:38 +0800

    fix p2p overlap

--- tests/multi_card_tests/pipeline_parallel/test_gpt_pp_with_moe.py
+++ tests/multi_card_tests/pipeline_parallel/test_gpt_pp_with_moe.py
@@ -95,7 +95,9 @@ def run_pp(
             "mp",
         ],
         "pp_configs": {
-            "forward_backward_overlap_scheduler": forward_backward_overlap_scheduler
+            "forward_backward_overlap_scheduler": forward_backward_overlap_scheduler,
+            "overlap_p2p_comm": True,
+            "enable_dynamic_shape": True,
         },
     }
     micro_batch_size = 1
