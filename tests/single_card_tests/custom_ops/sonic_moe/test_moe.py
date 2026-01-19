033b9d0e713a9bc89d374e8a3be755e5409d0691
ooooo <3164076421@qq.com>
2026-01-19 20:30:13 +0800

    apply review

--- tests/single_card_tests/custom_ops/sonic_moe/test_moe.py
+++ tests/single_card_tests/custom_ops/sonic_moe/test_moe.py
@@ -27,9 +27,7 @@ _SEED = 42
 
 import os
 
-RUN_IN_PADDLE_CI = bool(
-    paddle.utils.strtobool(os.getenv("RUN_IN_PADDLE_CI", "0"))
-)
+RUN_IN_PADDLE_CI = paddle.utils.strtobool(os.getenv("RUN_IN_PADDLE_CI", "0"))
 
 problem_shapes = [
     (8192, 768, 256, 128, 8),
