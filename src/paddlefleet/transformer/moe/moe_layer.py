dcb5c250c15b36d4b3a85f9b507f11d03309d5ae
AlAuAu <458134681@qq.com>
2026-01-19 13:05:34 +0800

    fix pp overlap with moe_grouped_gemm

--- src/paddlefleet/transformer/moe/moe_layer.py
+++ src/paddlefleet/transformer/moe/moe_layer.py
@@ -604,6 +604,8 @@ class MoELayer(nn.Layer):
                 self,
                 self.num_experts_per_tok,
                 use_fp8_mlp=self.fp8,
+                moe_deep_gemm=self.moe_deep_gemm,
+                moe_grouped_gemm=self.moe_grouped_gemm,
                 recompute_moe_gate_up=self.recompute_moe_gate_up,
                 recompute_moe_premute=self.recompute_moe_premute,
                 fp8_dispatched_handle=fp8_dispatched_handle,
