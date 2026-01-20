94e36023514667da8c3eb383986b0c530fbf9ef0
liuruyan <44316842+liuruyan@users.noreply.github.com>
2026-01-20 11:39:29 +0800

    【CP-UE8M0】Support fuse_stack_transpose_fp8_quant ue8m0 scale (#435)

--- tests/single_card_tests/custom_ops/test_ops_import.py
+++ tests/single_card_tests/custom_ops/test_ops_import.py
@@ -28,6 +28,7 @@ class TestOpsImport(unittest.TestCase):
         "fused_swiglu_scale_bwd",
         "fused_swiglu_bwd",
         "fuse_weighted_swiglu_fp8_quant",
+        "fuse_stack_transpose_fp8_quant",
     ]
 
     def setUp(self):
