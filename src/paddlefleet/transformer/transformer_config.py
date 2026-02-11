4b1807c9d853e105b51607483fb22efa68debaff
Nyako Shigure <shigure_nyako@outlook.com>
2026-02-11 15:12:49 +0800

    [release/0.2][ThirdParty] Cleanup `moe_use_pfcc_deepep` configuration (#547)
    Co-authored-by: Nyakku Shigure <sigure.qaq@gmail.com>

--- src/paddlefleet/transformer/transformer_config.py
+++ src/paddlefleet/transformer/transformer_config.py
@@ -313,9 +313,6 @@ class TransformerConfig(ModelParallelConfig):
     """The type of token dispatcher to use. The default is 'deepep'.
     Options are 'allgather','alltoall' and 'deepep'."""
 
-    moe_use_pfcc_deepep: bool = True
-    """Whether to use PFCC DeepEP for the MoE layer. If False, Paddle DeepEP is used. This argument takes effect only when moe_token_dispatcher_type is set to 'deepep'."""
-
     moe_use_fusion_node: bool = True
     """Whether to use fusion node for MoE layer. Default is True"""
 
