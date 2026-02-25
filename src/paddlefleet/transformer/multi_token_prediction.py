93ba97e8c5a9e9d8c13ea83cfef2866591c294db
Ting <wtmlon@foxmail.com>
2026-02-25 17:33:06 +0800

    add distillation loss.

--- src/paddlefleet/transformer/multi_token_prediction.py
+++ src/paddlefleet/transformer/multi_token_prediction.py
@@ -473,12 +473,12 @@ class MultiTokenPredictionLayer(FleetLayer):
 
         hidden_states_concat = dict_args["hidden_states"]
         # print("mtp layer num: ", self.layer_number)
-        for i in range(self.layer_number, 1):
+        for i in range(self.config.num_nextn_predict_layers):
             tensor_list = paddle.split(
                 hidden_states_concat, self.config.num_nextn_predict_layers + 1
             )
             dict_args["hidden_states"] = tensor_list[0]
-            dict_args["decoder_input"] = tensor_list[self.layer_number + 1]
+            dict_args["decoder_input"] = tensor_list[i + 1]
 
             if self.config.recompute_granularity == "full" and self.training:
                 hidden_states = self._checkpointed_forward(
@@ -487,7 +487,7 @@ class MultiTokenPredictionLayer(FleetLayer):
             else:
                 hidden_states = self._proj_and_transformer_layer(**dict_args)
 
-            tensor_list[self.layer_number + 1] = hidden_states
+            tensor_list[i + 1] = hidden_states
             hidden_states_concat = paddle.concat(tensor_list)
         dict_args["hidden_states"] = hidden_states_concat
         dict_args.pop("decoder_input")
