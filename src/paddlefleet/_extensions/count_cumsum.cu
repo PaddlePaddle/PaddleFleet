2028d11a130359643e083f69d797d1e17bb9284f
zrr1999 <2742392377@qq.com>
2026-02-27 11:47:42 +0800

    update

--- src/paddlefleet/_extensions/count_cumsum.cu
+++ src/paddlefleet/_extensions/count_cumsum.cu
@@ -248,7 +248,8 @@ std::vector<paddle::Tensor> CountCumsumCuda(const paddle::Tensor& x,
   }
 
   if (N == 0) {
-    // For N == 0, semantics require all-zero outputs instead of uninitialized memory.
+    // For N == 0, semantics require all-zero outputs instead of uninitialized
+    // memory.
     count_output.zero_();
     if (do_cumsum) {
       cumsum_output.zero_();
