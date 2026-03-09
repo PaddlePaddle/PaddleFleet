06dd54de6817b5278b2c9688f49da9820773b629
Severus Qin <qinhuapeng425@gmail.com>
2026-03-09 17:41:44 +0800

    move provider back to paddleformers

--- src/paddlefleet/models/qwen3_vl/__init__.py
+++ src/paddlefleet/models/qwen3_vl/__init__.py
@@ -18,11 +18,6 @@ from .qwen3_vl_model import (
     Qwen3VLVisionModel,
     Qwen3VLVisionTransformerLayer,
 )
-from .qwen3_vl_provider import (
-    Qwen3VLProvider,
-    Qwen3VLTextProvider,
-    Qwen3VLVisionProvider,
-)
 
 __all__ = [
     "Qwen3VLTextEmbedding",
@@ -30,7 +25,4 @@ __all__ = [
     "Qwen3VLTextTransformerLayer",
     "Qwen3VLVisionModel",
     "Qwen3VLVisionTransformerLayer",
-    "Qwen3VLVisionProvider",
-    "Qwen3VLProvider",
-    "Qwen3VLTextProvider",
 ]
