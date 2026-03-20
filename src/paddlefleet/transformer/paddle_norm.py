d5d9e8bba18c2b0a1204bbc0f6e4995bd5cdae0b
xiaoguoguo626807 <wangruting@baidu.com>
2026-03-20 03:07:37 +0000

    delte something
    delte something
    
    delte something
    
    delte something

--- src/paddlefleet/transformer/paddle_norm.py
+++ src/paddlefleet/transformer/paddle_norm.py
@@ -210,8 +210,6 @@ class WrappedPaddleNormPipe(paddle.nn.Layer):
                 [rst["hidden_states"], *tensor_list[1:]]
             )
             rst["hidden_states"] = hidden_states_concat
-
-        rst = {**dict_args, **rst}
         return rst
 
     def build_schedule_node(self):
