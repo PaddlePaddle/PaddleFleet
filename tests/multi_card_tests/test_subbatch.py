6f2b1a06dffb306e5647e76d49e6a5c6401982e6
tianlef <1095012807@qq.com>
2026-03-26 18:52:49 +0800

    fix subbatch

--- tests/multi_card_tests/test_subbatch.py
+++ tests/multi_card_tests/test_subbatch.py
@@ -29,6 +29,18 @@ Run with:
       tests/multi_card_tests/test_subbatch.py
 """
 
+import os
+import sys
+
+sys.path.insert(
+    0,
+    os.path.dirname(
+        os.path.dirname(
+            os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        )
+    ),
+)
+
 import functools
 import random
 import unittest
