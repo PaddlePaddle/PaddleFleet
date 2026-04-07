Fix SwiGLU precision regression and add coverage

src/paddlefleet/fusions/fused_bias_swiglu.py
@@ -18,6 +18,7 @@
 import logging
 
 import paddle
+import paddle.nn.functional as F
 
 from paddlefleet.jit import jit_fuser
 from paddlefleet.utils import nvtx_decorator
@@ -37,7 +38,8 @@ def swiglu(y):
     Returns:
         paddle.Tensor: Result of SwiGLU activation: SiLU(y1) * y2, where y1, y2 are the split halves.
     """
-    return paddle.nn.functional.swiglu(y)
+    y_1, y_2 = paddle.chunk(y, 2, -1)
+    return F.silu(y_1) * y_2
 
 
 @jit_fuser