f045a6d2c767de858de06f170e268e27043faf6e
DanielSun11 <1395924413@qq.com>
2026-04-10 14:53:58 +0800

    update paddle version

--- build_utils.py
+++ build_utils.py
@@ -224,7 +224,7 @@ def get_special_build_deps():
         major = sys.version_info.major
         minor = sys.version_info.minor
         deps = [
-            "paddlepaddle-gpu==3.3.1.post20260408+f85d56c9f33",
+            "paddlepaddle-gpu==3.3.1.post20260409+52c898ee9ac",
         ]
         # for deep_ep build
         if cuda_major == 12:
