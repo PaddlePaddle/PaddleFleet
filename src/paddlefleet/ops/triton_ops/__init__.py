e81933e6e14b437fd09e34705f49d3ceb3b495e3
ZhenxingLi <lizhenxing02@baidu.com>
2026-04-28 12:16:11 +0800

    Update __init__.py

--- src/paddlefleet/ops/triton_ops/__init__.py
+++ src/paddlefleet/ops/triton_ops/__init__.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from .rms_norm_fusion import RMSNormFusionTriton
 from .moe_topk_fusion import MoETopkFusion, routing_map_fusion_forward
 
 __all__ = [
