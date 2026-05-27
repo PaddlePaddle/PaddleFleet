c9227a88dc1865d241f5c42492e5235a9bed144e
xingmingyyj <zxm_3791@163.com>
2026-05-27 15:19:25 +0800

    fix

--- src/paddlefleet/transformer/moe/moe_layer.py
+++ src/paddlefleet/transformer/moe/moe_layer.py
@@ -1038,7 +1038,7 @@ class MoELayer(nn.Layer):
 
         _log_moe_md5(output, "moe_routed_output", layer_idx)
 
-        if self.training and self.router_aux_loss_coef:
+        if self.training and self.router_aux_loss_coef and aux_loss is not None:
             aux_loss = aux_loss * float(self.router_aux_loss_coef)
             output = AddAuxiliaryLoss.apply(output, aux_loss)
 
