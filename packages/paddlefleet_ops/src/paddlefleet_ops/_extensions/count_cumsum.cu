d5fe25420b00646666529030f14de73b1b12a8da
zhengshengning <ningzhengsheng@baidu.com>
2026-06-04 15:20:23 +0800

    fix

--- packages/paddlefleet_ops/src/paddlefleet_ops/_extensions/count_cumsum.cu
+++ packages/paddlefleet_ops/src/paddlefleet_ops/_extensions/count_cumsum.cu
@@ -238,13 +238,18 @@ std::vector<paddle::Tensor> CountCumsumCuda(const paddle::Tensor& x,
   auto place = x.place();
   cudaStream_t stream = x.stream();
 
-  auto count_output = paddle::empty({E}, paddle::DataType::INT32, place);
+  auto count_output = paddle::full({E}, 0, paddle::DataType::INT32, place);
 
   paddle::Tensor cumsum_output;
   if (do_cumsum) {
-    cumsum_output = paddle::empty({E}, paddle::DataType::INT32, place);
+    cumsum_output = paddle::full({E}, 0, paddle::DataType::INT32, place);
   } else {
     cumsum_output = paddle::empty({0}, paddle::DataType::INT32, place);
+  }
+
+  // 0-size input: outputs are all zeros, skip the cooperative kernel launch.
+  if (N == 0) {
+    return {count_output, cumsum_output};
   }
 
   if (x.dtype() == paddle::DataType::INT32) {
