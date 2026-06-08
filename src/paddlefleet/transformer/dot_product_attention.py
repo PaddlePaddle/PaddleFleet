d205df567994a8d7d685c4ee753f14087061de9f
xxyux <1650459510@qq.com>
2026-06-08 11:32:25 +0800

    fix expand_attn_mask_startend_row_indices_for_cp for dim=2

--- src/paddlefleet/transformer/dot_product_attention.py
+++ src/paddlefleet/transformer/dot_product_attention.py
@@ -281,30 +281,13 @@ class DotProductAttention(FleetLayer):
                 [attn_mask_startend_row_indices, append_indices_expand],
                 axis=-1,
             )
-        elif attn_mask_startend_row_indices.shape[-1] == 2:
-            if self.config.experimental_dataflow:
-                # In EB dataflow, attn_mask_startend_row_indices.shape[-1] == 2
-                # means attn_mask_startend_row_indices is ready, do not need to concat
-                pass
-            else:
-                b, k_heads, k_seqlen, _ = attn_mask_startend_row_indices.shape
-                append_indices = paddle.to_tensor(
-                    np.arange(seq_len),
-                    dtype=attn_mask_startend_row_indices.dtype,
-                )
-                append_indices = append_indices.reshape(1, 1, seq_len, 1)
-                append_indices_expand0 = append_indices.expand(
-                    b, k_heads, k_seqlen, 1
-                )
-                append_indices_expand1 = append_indices_expand0.clone()
-                attn_mask_startend_row_indices = paddle.concat(
-                    [
-                        attn_mask_startend_row_indices,
-                        append_indices_expand0,
-                        append_indices_expand1,
-                    ],
-                    axis=-1,
-                )
+        elif (
+            attn_mask_startend_row_indices.shape[-1] == 2
+            and self.config.experimental_dataflow
+        ):
+            # In EB dataflow, attn_mask_startend_row_indices.shape[-1] == 2
+            # means attn_mask_startend_row_indices is ready, do not need to concat
+            pass
         else:
             raise ValueError(
                 "Invalid attention mask shape, when using context parallel, attn_mask_startend_row_indices.shape[-1] must be either 1 or 2"
