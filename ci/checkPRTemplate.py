588223ecc35b187740ef8912644bd2095aa19e88
tianlef <1095012807@qq.com>
2026-06-09 19:50:32 +0800

    fix pr template

--- ci/checkPRTemplate.py
+++ ci/checkPRTemplate.py
@@ -28,7 +28,7 @@ if BRANCH.startswith("develop"):
     }
 elif BRANCH.startswith("release"):
     REPO_TEMPLATE = {
-        "PaddleFleet": r"""### PR Category(.*[^\s].*)### PR Types(.*[^\s].*)### Description(.*?devPR:https://github\.com/PaddlePaddle/PaddleFleet/pull/.*?)(?:\n###|\Z)"""
+        "PaddleFleet": r"""### PR Category(.*[^\s].*)### PR Types(.*[^\s].*)### Description(.*[^\s].*)"""
     }
 
 
@@ -97,14 +97,6 @@ def parameter_accuracy(body):
                     single_mess += f"{i}."
             if len(single_mess) != 0:
                 message += f"{key} should be in {test_list}. but now is [{single_mess}]."
-    if BRANCH.startswith("release"):
-        PR_dic["Description"] = body[changes_end + len("### Description") :]
-        des_pr_id = extract_pr_links(PR_dic["Description"])
-        if len(des_pr_id) == 0 or not check_link_accessible(
-            "https://github.com/PaddlePaddle/PaddleFleet/pull/"
-            + str(des_pr_id[0])
-        ):
-            message += "The PR link does not exist. To merge into the release branch, you need to merge into the develop branch first and then cherry-pick it to the release/3.3 branch. Please merge into develop first and fill in the PR link in the Description.Use this format devPR:https://github.com/PaddlePaddle/PaddleFleet/pull/xxxx"
     return message
 
 
@@ -142,8 +134,7 @@ def checkPRTemplate(repo, body, CHECK_TEMPLATE):
     elif result is None:
         res = False
         message = parameter_accuracy(body)
-        if BRANCH.startswith("release") and len(message) == 0:
-            message = "The PR link does not exist. To merge into the release/3.3 branch, you need to merge into the develop branch first and then cherry-pick it to the release/3.3 branch. Please merge into develop first and fill in the PR link in the Description.Use this format devPR:https://github.com/PaddlePaddle/Paddle/pull/xxxx"
+        print(message)
     return res, message
 
 
