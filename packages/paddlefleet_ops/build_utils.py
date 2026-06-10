2bf7dce8c0a987aa2fd0fea457194c0514508706
gushiwei <gushiwei@baidu.com>
2026-06-10 04:13:09 +0000

    rebuild

--- packages/paddlefleet_ops/build_utils.py
+++ packages/paddlefleet_ops/build_utils.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# trigger rebuild for 0.4dev
+
 from __future__ import annotations
 
 import logging
