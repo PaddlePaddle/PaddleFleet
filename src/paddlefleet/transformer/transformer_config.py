be32d9094d050309367930607f09824fa1b13e07
zhangbo9674 <zhangbo54@baidu.com>
2026-06-11 19:17:13 +0800

    fix

--- src/paddlefleet/transformer/transformer_config.py
+++ src/paddlefleet/transformer/transformer_config.py
@@ -641,7 +641,7 @@ class TransformerConfig(ModelParallelConfig):
     high_precision_mhc: bool = True
     """Use high precision (float32) for mHC forward and backward computation."""
 
-    high_precision_compressor: bool = True
+    high_precision_compressor: bool = False
     """Use high precision (float32) for compressor forward and backward computation."""
 
     ####################
