f6aefcea609880aecf3e57f0d95f989930359dcc
gushiwei <gushiwei@baidu.com>
2026-06-13 05:05:48 +0000

    update decision

--- tests/single_card_tests/model/test_gpt_model_moe_grouped_gemm.py
+++ tests/single_card_tests/model/test_gpt_model_moe_grouped_gemm.py
@@ -189,8 +189,8 @@ class TestGPTModel(unittest.TestCase):
         repo_name = os.environ.get("repo_flag")
         if judge_machine_type() == "H":
             if version == 13:
-                assert loss.item() == 5.239129066467285, (
-                    f"loss not equal ({loss.item()} != 5.239129066467285), please check your modify"
+                assert loss.item() == 5.239296913146973, (
+                    f"loss not equal ({loss.item()} != 5.239296913146973), please check your modify"
                 )
                 assert embed_tokens_grad_norm == 2.796875, (
                     f"grad norm of embed_tokens not equal ({embed_tokens_grad_norm} != 2.796875), please check your modify"
@@ -204,8 +204,8 @@ class TestGPTModel(unittest.TestCase):
                         f"grad norm of embed_tokens not equal ({embed_tokens_grad_norm} != 2.796875), please check your modify"
                     )
                 else:  # 12.9
-                    assert loss.item() == 5.239129066467285, (
-                        f"loss not equal ({loss.item()} != 5.239129066467285), please check your modify"
+                    assert loss.item() == 5.239296913146973, (
+                        f"loss not equal ({loss.item()} != 5.239296913146973), please check your modify"
                     )
                     assert embed_tokens_grad_norm == 2.796875, (
                         f"grad norm of embed_tokens not equal ({embed_tokens_grad_norm} != 2.796875), please check your modify"
