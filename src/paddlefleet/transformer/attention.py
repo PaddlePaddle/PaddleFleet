28a02914f2b3b1205e0eaafd88af23a3d77a091c
GuoxiaWang <mingzilaochongtu@gmail.com>
2026-06-16 00:05:10 +0800

    add unit test

--- src/paddlefleet/transformer/attention.py
+++ src/paddlefleet/transformer/attention.py
@@ -1465,7 +1465,7 @@ class SelfAttentionVHA(Attention):
         )
         query = self._apply_vha_premix(query)  # -> [b, sq, nh, hd]
 
-        if self.shared_kv is True:
+        if self.shared_kv:
             norm_key = shared_kv
         else:
             norm_key = key
