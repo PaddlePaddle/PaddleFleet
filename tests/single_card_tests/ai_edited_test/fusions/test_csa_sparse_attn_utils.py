3dcc9f0e4fa890f342fa5386b8a7476426a4ed3e
xxyux <1650459510@qq.com>
2026-07-28 14:26:23 +0800

    [Perf] CSA sparse attn backward: pass per-query topk_length + bump cudnn-frontend

--- tests/single_card_tests/ai_edited_test/fusions/test_csa_sparse_attn_utils.py
+++ tests/single_card_tests/ai_edited_test/fusions/test_csa_sparse_attn_utils.py
@@ -321,6 +321,7 @@ class TestCudnnBackendDispatch(unittest.TestCase):
             attn_sink,
             topk_idxs_flat,
             softmax_scale=None,
+            topk_length=None,
         ):
             return (
                 paddle.ones_like(q_flat),
